ab2b3c21cdc5689a4039bc4150197a0ba54697af
Idelfonso Joas <idelfonsojoas@gmail.com>
2022-08-11 23:09:14 -0300

    create model planet

---
+++ lib/models/planet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Planet {
+  final String id;
+  final String name;
+  final String image;
+  final String description;
+  final Color boxShadow;
+  final List<Color> colors;
+
+  const Planet({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.boxShadow,
+    required this.colors,
+  });
+}

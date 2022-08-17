f79d22dce8c762829f29f1286cc66d14a2c3cb40
Idelfonso Joas <idelfonsojoas@gmail.com>
2022-08-17 09:23:39 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:solar_system/pages/get_started.dart';
 import 'package:solar_system/pages/home.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:solar_system/pages/planet_details.dart';
 import './utils/routes.dart';
 
 int? isViewed;
 
 void main() async {
-  SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
   final prefs = await SharedPreferences.getInstance();
 
@@ -27,25 +27,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Solar System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
         textTheme: const TextTheme(
-            headline1: TextStyle(
-          fontFamily: 'Poppins',
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-          fontSize: 42,
-          decoration: TextDecoration.none,
-        )),
+          headline1: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold,
+            color: Color(0xff020632),
+            fontSize: 48,
+            decoration: TextDecoration.none,
+          ),
+          headline2: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 42,
+            decoration: TextDecoration.none,
+          ),
+          headline3: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            fontSize: 18,
+            decoration: TextDecoration.none,
+          ),
+          headline4: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 14,
+            color: Color(0xff6C6C6C),
+            decoration: TextDecoration.none,
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: isViewed != 1 ? Routes.getStarted : Routes.home,
       routes: {
         Routes.getStarted: (context) => const GetStarted(),
         Routes.home: (context) => const Home(),
+        Routes.planetDetails: (context) => const PlanetDetails(),
       },
     );
   }
